ec780dc96c15ef857a76c405cc5d254869e7e8b4
KaiyoteSoft <Spikeyhedgehog141@gmail.com>
2015-02-16 15:46:11 -0800

    Added monsters and collision detection for the bullets

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import pygame, sys
+import pygame, sys, random
 import math
 import time
 from pygame.locals import *
@@ -15,8 +15,13 @@ player_circle = pygame.image.load("img/final_circle.png")
 # player_rect = player_circle.get_rect()
 BLUE = ((0, 0, 255))
 clock = pygame.time.Clock()
-bullet_delay = 5
+bullet_delay = 10
 
+# class Player(pygame.sprite.Sprite):
+#     def __init__(self):
+#         pygame.sprite.Sprite.__init__(self)
+#         self.image = pygame.image.load("img/final_circle.png")
+#         self.rect = self.image.get_rect(center = (640, 385))
 
 def get_angle(pos, control_center):
     center = control_center
@@ -82,10 +87,61 @@ class Bullet(pygame.sprite.Sprite):
         self.rect.centery = int(self.center_y - opposite)
         self.hypotenuse += 8
 
+class Monster(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load("img/square.png")
+        self.rect = self.image.get_rect()
+
+        self.safe_surf = pygame.Surface((1280, 770))
+        self.safe_zone = self.safe_surf.get_rect()
+        self.safe_zone.center = (640, 385)
+
+    ### checks to see if monsters are created within safe zone and if so they are redrawn
+        self.in_safe = False
+        while not self.in_safe:
+            self.rect.right = random.randrange(-100, 1380)
+            self.rect.top = random.randrange(-300, 0)
+            if self.rect.colliderect(self.safe_zone):
+                pass
+            else:
+                self.in_safe = True
+
+        self.speed = 3
+
+    def update(self, player_centerx, player_centery):
+        self.centerx = player_centerx
+        self.centery = player_centery
+        x = self.centerx
+        y = self.centery
+        if self.rect.centerx > x:
+            self.rect.centerx -= self.speed
+        if self.rect.centerx < x:
+            self.rect.centerx += self.speed
+        if self.rect.centery > y:
+            self.rect.centery -= self.speed
+        if self.rect.centery < y:
+            self.rect.centery += self.speed
+
+def uncrowd(mon_group):
+    temp_group = mon_group.copy()
+    for mon in mon_group:
+        temp_group.remove(mon)
+        if pygame.sprite.spritecollideany(mon, temp_group):
+            mon.rect.centerx += random.randint(-10, 10)
+            mon.rect.centery += random.randint(-10, 10)
+
+
 
 ### variables and creating the group for bullets
 bullet_group = pygame.sprite.Group()
 bullet_timer = bullet_delay
+# character = Player()
+
+monster_group = pygame.sprite.Group()
+for number in range(0, 8):
+    monster = Monster()
+    monster_group.add(monster)
 
 if android:
     android.init()
@@ -123,9 +179,6 @@ while True:
 
     mouse_pos = pygame.mouse.get_pos()
 
-        # elif event.type == MOUSEBUTTONDOWN:
-        #     mouse_pos = pygame.mouse.get_pos()
-
 
 ### x-axis acceleration
     if velocity_reading[1] > 1 and x < 1280:
@@ -161,25 +214,37 @@ while True:
             bullet_timer = bullet_timer - 1
 
         end_x, end_y = beam(angle, player_rect.centerx, player_rect.centery)
-        pygame.draw.line(windowSurface, BLUE, (player_rect.centerx, player_rect.centery), (end_x, end_y))
+        # pygame.draw.line(windowSurface, BLUE, (player_rect.centerx, player_rect.centery), (end_x, end_y))
         # print(angle)
 
     control_circle_small = pygame.draw.circle(windowSurface, BLUE, (1080, 550), 10, 2)
     control_circle = pygame.draw.circle(windowSurface, WHITE, (1080, 550), 100, 4)
 
-    # if bullet_rect.centerx > 1290 or bullet_rect.centerx < 0:
-    #     bullet_group.remove(bullet)
-    #     print(bullet_group)
+### drawing the monsters and applying uncrowd algorithm
+    player_rect = player_circle.get_rect(center = (x, y))
+    monster_group.update(player_rect.centerx, player_rect.centery)
+    monster_group.draw(windowSurface)
+    uncrowd(monster_group)
 
 
 ### drawing the bullets
     bullet_group.update()
+    for bullet in bullet_group:
+        if bullet.rect.centerx > 1280 or bullet.rect.centerx < 0\
+                or bullet.rect.centery > 770 or bullet.rect.centery < 0:
+            bullet_group.remove(bullet)
     bullet_group.draw(windowSurface)
 
 ### blitting the player
     player_rect = player_circle.get_rect(center = (x, y))
     windowSurface.blit(player_circle, player_rect)
 
+### collision detection for the bullet, player, and monster
+    pygame.sprite.groupcollide(bullet_group, monster_group, True, True)
+
+    # for monster in monster_group:
+    #     if player_circle.collidepoint(monster):
+    #         print("collision")
 
     clock.tick(60)
     pygame.display.update()
